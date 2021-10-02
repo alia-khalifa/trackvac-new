63452f4a57d446aa1d498277cd0add5d74af1f9b
ahmad samir <ahmadabuzaiid@gmail.com>
2021-10-02 02:58:08 +0200

    final v1.2

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({ path: __dirname + '/.env' });
 
-const dbUrl = 'mongodb://localhost:27017/trackvac';
+const dbUrl = 'mongodb+srv://ahmadabuzaiid:atlasdatapassword@trackvac.zczmv.mongodb.net/trackvac?retryWrites=true&w=majority';
 
 const db = mongoose.connection;
 
